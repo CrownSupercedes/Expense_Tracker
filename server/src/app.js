dbfffc2bb09903954f9837fdae513bb0f36dfd4c
CrownSupercedes <adedaratech@gmail.com>
2023-12-03 16:13:46 +0000

    Users controller

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 const express = require("express");
-
+const dbConnect = require("./config/dbConnect");
+const registerUser = require("./controllers/users/users_controller");
 const app = express();
 
+//dbConnect
+dbConnect();
 
+//routes
+app.post("/register", registerUser);
 module.exports = app;
---
+++ server/src/controllers/users/users_controller.js
@@ -0,0 +1,6 @@
+//Register
+const registerUser = (req, res) => {
+	res.json({ user: "admin" });
+};
+
+module.exports = registerUser;
